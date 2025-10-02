a98b4e9a85190755e26bbf2bc2e4c89de47c17d1
Dheeraj Gupta <dheeraj200011@gmail.com>
2025-10-02 16:42:47 +0530

    done

--- Linked-List/removeNthNode.js
+++ Linked-List/removeNthNode.js
@@ -1,3 +1,6 @@
+// example : [1,2,3,4,5,6] n = 2
+// output: [[1,2,3,4,6] // last index jo n ki value hai wo remove hoga
+
 function Node(value) {
   this.value = value;
   this.next = null;
@@ -12,39 +15,44 @@ function LinkedList() {
 
 LinkedList.prototype.add = function (value) {
   let newNode = new Node(value);
+
   if (!this.head) {
     this.head = newNode;
     return;
   }
+
   let current = this.head;
   while (current.next) {
     current = current.next;
   }
+
   current.next = newNode;
 };
 
-// remove elements
+// remove nth elemnts
 
-function removeElemnts(head, value) {
-  // isme hum new empty list node ko bnayenge
+function removeNthElements(head, n) {
   let sentinal = new Node(0);
   sentinal.next = head;
-  let prev = sentinal;
-  while (prev.next) {
-    if (prev.next.value === value) {
-      prev.next = prev.next.next;
-    } else {
-      prev = prev.next;
-    }
+  let length = 0;
+  while (head) {
+    head = head.next;
+    length++;
   }
+  let prevPos = length - n;
+  let prev = sentinal;
+  for (let i = 0; i < prevPos; i++) {
+    prev = prev.next;
+  }
+  prev.next = prev.next.next;
   return sentinal.next;
 }
 
 let list = new LinkedList();
 list.add(1);
 list.add(2);
-list.add(6);
+list.add(3);
 list.add(4);
 list.add(6);
 
-console.log(removeElemnts(list.head, 6));
+console.log(removeNthElements(list.head, 2));

cba513be9519990c384afe6a84b4872d315e3d28
Dheeraj Gupta <dheeraj200011@gmail.com>
2026-01-28 12:54:12 +0530

    dp completed

--- Dynamic Programming/unique-paths.js
+++ Dynamic Programming/unique-paths.js
@@ -1,2 +1,19 @@
 // unique paths
+// isme hum star ke direction se chalenge
 
+const uniquePaths = (m, n) => {
+  let dp = Array.from({ length: m }, () => Array(n).fill(-1));
+  let fn = (x, y) => {
+    if (x == 0 && y === 0) return 1;
+    if (x < 0 || y < 0) return 0;
+
+    if (dp[x][y] != -1) return dp[x][y];
+    // fromula
+    dp[x][y] = fn(x - 1, y) + fn(x, y - 1);
+    return dp[x][y];
+  };
+
+  return fn(m - 1, n - 1);
+};
+
+console.log(uniquePaths(3, 7));
